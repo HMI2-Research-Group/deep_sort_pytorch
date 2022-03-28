f1e7b66f8f5ce61556479083e37be48e19994954
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2022-03-27 18:31:36 -0700

    sync

---
+++ dependency_install.sh
@@ -0,0 +1,24 @@
+!#/bin/bash
+conda install -y pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+conda install -y -c anaconda scipy
+conda install -y -c anaconda cython
+conda install -y -c conda-forge scikit-learn
+conda install -y -c conda-forge matplotlib
+conda install -y -c conda-forge ros-rospy
+conda install -y -c conda-forge ros-geometry-msgs
+conda install -y -c anaconda pillow
+conda install -y -c conda-forge easydict
+conda install -y -c conda-forge pyyaml
+conda install -y -c conda-forge tabulate
+conda install -y -c conda-forge yacs
+pip install opencv-python
+pip install opencv-contrib-python
+pip install Vizer
+pip install faiss-cpu
+pip install tensorboard
+pip install gdown
+pip install termcolor
+pip install openmim
+mim install mmdet
+
+
