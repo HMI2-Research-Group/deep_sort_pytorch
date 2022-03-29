4a7de04baaadba3f6aafcc3d18f3fd65029c2873
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2022-03-28 19:06:58 -0700

    Excellent Progress

--- deepsort_ros.py
+++ deepsort_ros.py
@@ -21,121 +21,9 @@ from utils.log import get_logger
 from utils.io import write_results
 
 
-class VideoTracker(object):
-    def __init__(self, cfg, args, video_path):
-        self.cfg = cfg
-        self.args = args
-        self.video_path = video_path
-        self.logger = get_logger("root")
-
-        use_cuda = args.use_cuda and torch.cuda.is_available()
-        if not use_cuda:
-            warnings.warn("Running in cpu mode which maybe very slow!", UserWarning)
-
-        if args.display:
-            cv2.namedWindow("test", cv2.WINDOW_NORMAL)
-            cv2.resizeWindow("test", args.display_width, args.display_height)
-
-        if args.cam != -1:
-            print("Using webcam " + str(args.cam))
-            self.vdo = cv2.VideoCapture(args.cam)
-        else:
-            self.vdo = cv2.VideoCapture()
-        self.detector = build_detector(cfg, use_cuda=use_cuda)
-        self.deepsort = build_tracker(cfg, use_cuda=use_cuda)
-        self.class_names = self.detector.class_names
-
-    def __enter__(self):
-        if self.args.cam != -1:
-            ret, frame = self.vdo.read()
-            assert ret, "Error: Camera error"
-            self.im_width = frame.shape[0]
-            self.im_height = frame.shape[1]
-
-        else:
-            assert os.path.isfile(self.video_path), "Path error"
-            self.vdo.open(self.video_path)
-            self.im_width = int(self.vdo.get(cv2.CAP_PROP_FRAME_WIDTH))
-            self.im_height = int(self.vdo.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            assert self.vdo.isOpened()
-
-        if self.args.save_path:
-            os.makedirs(self.args.save_path, exist_ok=True)
-
-            # path of saved video and results
-            self.save_video_path = os.path.join(self.args.save_path, "results.avi")
-            self.save_results_path = os.path.join(self.args.save_path, "results.txt")
-
-            # create video writer
-            fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-            self.writer = cv2.VideoWriter(self.save_video_path, fourcc, 20, (self.im_width, self.im_height))
-
-            # logging
-            self.logger.info("Save results to {}".format(self.args.save_path))
-
-        return self
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        if exc_type:
-            print(exc_type, exc_value, exc_traceback)
-
-    def run(self):
-        results = []
-        idx_frame = 0
-        while self.vdo.grab():
-            idx_frame += 1
-            if idx_frame % self.args.frame_interval:
-                continue
-
-            start = time.time()
-            _, ori_im = self.vdo.retrieve()
-            im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
-
-            # do detection
-            bbox_xywh, cls_conf, cls_ids = self.detector(im)
-
-            # select person class
-            mask = cls_ids == 0
-
-            bbox_xywh = bbox_xywh[mask]
-            # bbox dilation just in case bbox too small, delete this line if using a better pedestrian detector
-            bbox_xywh[:, 3:] *= 1.2
-            cls_conf = cls_conf[mask]
-
-            # do tracking
-            outputs = self.deepsort.update(bbox_xywh, cls_conf, im)
-
-            # draw boxes for visualization
-            if len(outputs) > 0:
-                bbox_tlwh = []
-                bbox_xyxy = outputs[:, :4]
-                identities = outputs[:, -1]
-                ori_im = draw_boxes(ori_im, bbox_xyxy, identities)
-
-                for bb_xyxy in bbox_xyxy:
-                    bbox_tlwh.append(self.deepsort._xyxy_to_tlwh(bb_xyxy))
-
-                results.append((idx_frame - 1, bbox_tlwh, identities))
-
-            end = time.time()
-
-            if self.args.display:
-                cv2.imshow("test", ori_im)
-                cv2.waitKey(1)
-
-            if self.args.save_path:
-                self.writer.write(ori_im)
-
-            # save results
-            if self.args.save_path:
-                write_results(self.save_results_path, results, "mot")
-
-            # logging
-            self.logger.info(
-                "time: {:.03f}s, fps: {:.03f}, detection numbers: {}, tracking numbers: {}".format(
-                    end - start, 1 / (end - start), bbox_xywh.shape[0], len(outputs)
-                )
-            )
+def disp_image(image):
+    cv2.imshow("debug", image)
+    cv2.waitKey(0)
 
 
 class ROS_VideoTracker(object):
@@ -162,6 +50,33 @@ class ROS_VideoTracker(object):
     def depth_callback(self, msg):
         self.depth_stream = self.bridge.imgmsg_to_cv2(msg)
 
+    def get_depth_from_pixels(self, ori_depth, bbox_outputs):
+        def recursive_non_nan_search(depth, x, y):
+            # Recursively search for non-nan values around the point (x, y)
+            if np.isnan(depth[y, x]):
+                if x > 0:
+                    return recursive_non_nan_search(depth, x - 1, y)
+                elif y > 0:
+                    return recursive_non_nan_search(depth, x, y - 1)
+            else:
+                return depth[y, x]
+
+        # TODO: Initilize this 100 as a parameter
+        human_positions = np.zeros((100, 3))
+        for i in range(len(bbox_outputs)):
+            xmin = bbox_outputs[i][0]
+            ymin = bbox_outputs[i][1]
+            xmax = bbox_outputs[i][2]
+            ymax = bbox_outputs[i][3]
+            human_index = bbox_outputs[i][4]
+            mid_x, mid_y = (xmin + xmax) / 2, (ymin + ymax) / 2
+            human_positions[int(human_index)] = (
+                int(mid_x),
+                int(mid_y),
+                recursive_non_nan_search(ori_depth, int(mid_x), int(mid_y)),
+            )
+        return human_positions
+
     def run(self):
         while not rospy.is_shutdown():
             results = []
@@ -173,6 +88,7 @@ class ROS_VideoTracker(object):
 
                 start = time.time()
                 ori_im = self.rgb_stream
+                ori_depth = self.depth_stream
                 im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
 
                 # do detection
@@ -200,6 +116,7 @@ class ROS_VideoTracker(object):
                         bbox_tlwh.append(self.deepsort._xyxy_to_tlwh(bb_xyxy))
 
                     results.append((idx_frame - 1, bbox_tlwh, identities))
+                    all_pedestrian_depth = self.get_depth_from_pixels(ori_depth, outputs)
 
                 end = time.time()
 
