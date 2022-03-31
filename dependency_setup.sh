98c633b36070ec395d63f2f431034dde7c1e879c
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2022-03-30 19:54:51 -0700

    sync

--- dependency_setup.sh
+++ dependency_setup.sh
@@ -8,6 +8,12 @@ mkdir src && cd src
 git clone -b melodic-devel git@github.com:ros/geometry2.git
 git clone -b melodic-devel git@github.com:ros/geometry.git
 cd ..
+# Basically we will be using everything from ROS, except the Python interpreter which we will be using
+# from anaconda. Then in our code we will be adding the dist-packages to the PYTHONPATH
+# Example: sys.path.append("/tmp/catkin_ws/devel/lib/python3/dist-packages")
+# Pro Tip: If you do this in a normal folder rather than temp folder, even Pylance can recognize the Python packages
+# and it will be able to import them.
+source /opt/ros/melodic/setup.bash
 catkin_make --cmake-args \
             -DCMAKE_BUILD_TYPE=Release \
             -DPYTHON_EXECUTABLE=$HOME/anaconda3/envs/deepsort/bin/python \
